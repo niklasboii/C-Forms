3abd1b4d0892d87344147e48c201de55a866cf54
Niklas Eloranta <98732864+niklasboii@users.noreply.github.com>
2022-11-11 14:44:08 +0200

    Add files via upload

---
+++ 1 Harjoitus/1 Harjoitus/Form1.cs
@@ -0,0 +1,20 @@
+namespace _1_Harjoitus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void VaihdaBT_Click(object sender, EventArgs e)
+        {
+            OtsikkoLB.Text = "Heippa maailma";
+        }
+    }
+}
