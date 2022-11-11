11bb5df1708f3c366690c37a2816bc57f6e7afa8
Niklas Eloranta <98732864+niklasboii@users.noreply.github.com>
2022-11-11 14:43:30 +0200

    Add files via upload

---
+++ 2 Harjoitus/2 Harjoitus/Form1.cs
@@ -0,0 +1,22 @@
+namespace _2_Harjoitus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string teksti = ViestiTB.Text;
+            TulostusLB.Text = teksti;
+            TulostusLB.Visible = true;
+        }
+    }
+}
